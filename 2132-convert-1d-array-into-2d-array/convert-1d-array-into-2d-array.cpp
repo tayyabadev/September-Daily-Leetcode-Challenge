508b4f3bb0f08e9e463b4d2fc5325c4310bff3bd
Tayyaba Tabassum <142013549+tayyabadev@users.noreply.github.com>
2024-09-01 16:17:03 +0500

    Time: 92 ms (21.17%) | Memory: 92.7 MB (25.95%) - LeetSync

---
+++ 2132-convert-1d-array-into-2d-array/convert-1d-array-into-2d-array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> construct2DArray(vector<int>& original, int m, int n) {
+        vector<vector<int>> res(m, vector<int>(n));
+        if (m * n != original.size()) {
+            return {};
+        }
+        int ptr = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[i][j] = original[ptr++];
+            }
+        }
+        return res;
+    }
+};
